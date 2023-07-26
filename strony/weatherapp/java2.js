29e6536a49327ff6398afd435df135e710d81606
MasnyGul <136578356+MasnyGul@users.noreply.github.com>
2023-07-26 23:53:09 +0200

    Add files via upload

--- strony/weatherapp/java2.js
+++ strony/weatherapp/java2.js
@@ -1,38 +1,38 @@
-const container=document.querySelector('.container');
-const search=document.querySelector('.search-box button');
-const weatherBox=document.querySelector('.weather-box');
-const weatherdetails=document.querySelector('.weather-details');
-const error404=document.querySelector('.not-found');
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey='6e101b772dbd3fa94d4e82690633ea46';
-    const city=document.querySelector('.search-box input').value;
+    const APIKey = '6e101b772dbd3fa94d4e82690633ea46';
+    const city = document.querySelector('.search-box input').value;
 
-    if(city === '')
+    if (city === '')
         return;
-    
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-        .then(response =>response.json())
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
         .then(json => {
-            if(json.cod === '404'){
-                container.style.height ='400px';
-                weatherBox.style.display='none';
-                weatherdetails.style.display='none';
-                error404.style.display='block';
+            console.log(json);
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
                 error404.classList.add('fadeIn');
                 return;
             }
 
-            error404.style.display='none';
-            error404.classList.remove('fadeIn')
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
 
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
-
 
             switch (json.weather[0].main) {
                 case 'Clear':
@@ -57,9 +57,12 @@ search.addEventListener('click', () => {
 
                 default:
                     image.src = '';
-            };
+            }
+            
 
-            temperature.innerHTML = `10<span>°C</span>`;
+            const temp= 
+            
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
@@ -71,8 +74,7 @@ search.addEventListener('click', () => {
             container.style.height = '590px';
 
 
-
-
         });
+
 
 });

9131ae2a1a4e2b115c619b59a36112a38fb50116
MasnyGul <136578356+MasnyGul@users.noreply.github.com>
2023-08-05 23:54:59 +0200

    Add files via upload

---
+++ strony/array/index.js
@@ -0,0 +1,58 @@
+const btn=document.querySelector('button');
+const rez=document.querySelector('input');
+const odp=document.querySelector('p');
+const min=document.querySelector('#min');
+const pl=document.querySelector('#plus')
+const licz=document.querySelector('h5')
+const array=[];
+pl.addEventListener('click', () => {
+    
+    if(document.querySelector('h5').innerHTML<10)
+    {
+    if(rez.value!=''){
+    array.push(rez.value);    
+    rez.value='';
+    var liczba=document.querySelector('h5').innerHTML;
+    liczba++;
+    licz.innerHTML=liczba;
+    console.log(liczba);     
+    console.log(array) 
+    }
+    }
+    else 
+    rez.value='';  
+})
+
+min.addEventListener('click', () => {
+    rez.value='';
+    if(document.querySelector('h5').innerHTML>1)
+    {
+    array.pop();
+    var liczba=document.querySelector('h5').innerHTML;
+    liczba--;
+    licz.innerHTML=liczba;
+    console.log(liczba);
+    console.log(array) 
+    }
+    else
+    rez.value='';  
+    
+})
+
+
+
+btn.addEventListener('click', () => {
+    if(rez.value!='')
+    {
+    array.push(rez.value);
+    odp.innerHTML=array;
+    console.log(array);  
+    }
+    else
+    odp.innerHTML=array;
+
+    array.length=0;
+    console.log(array);
+    rez.value='';
+});
+

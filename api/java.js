d8101f2ca70aecd8d2bfd8b3f2d97747fa45cac7
MasnyGul <136578356+MasnyGul@users.noreply.github.com>
2023-08-08 23:55:52 +0200

    Delete api directory

--- api/java.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const response = fetch('https://api.openweathermap.org/data/2.5/weather?q=Warsaw&units=metric&appid=6e101b772dbd3fa94d4e82690633ea46');
-    const movies = response.json();
-    console.log(movies)
-    console.log("Hello World!");
-  });
